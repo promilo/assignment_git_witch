3522333f7b273a6fb06d7213c0c9e227640626b2
promilo <milind.shah@mail.utoronto.ca>
2017-11-08 15:02:06 -0500

    finished writing classes for question parser

---
+++ src/QuestionParser.js
@@ -0,0 +1,42 @@
+getUser = (input) => {
+
+  let splitInput = input.split(' ')
+  return splitInput[splitInput.length - 2]
+
+},
+getSubject = (input) => {
+  let splitInput = input.split(' ')
+  if (splitInput[2] === "starred"){
+    return "stars"
+  } else {
+    return "repos"
+  }
+
+},
+
+getQuery = (input) => {
+  let splitInput = input.split(' ')
+  if (splitInput[0] === 'how'){
+    return "count"
+  } else {
+    return "details"
+  }
+
+}
+
+class QuestionParser {
+  constructor() {
+    this.output = {}
+  }
+  parse(input){
+
+    this.output = {
+      username: getUser(input),
+      subject: getSubject(input),
+      query: getQuery(input)
+    }
+    return this.output
+  }
+}
+
+module.exports = QuestionParser
